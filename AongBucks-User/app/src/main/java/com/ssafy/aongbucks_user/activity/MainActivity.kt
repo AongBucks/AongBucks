4775d208a2f36186f17950ec2f3935f717399ebd
Jeehyun Woo <wjh51333@gmail.com>
2021-11-22 01:01:35 +0900

    로그아웃 추가

--- AongBucks-User/app/src/main/java/com/ssafy/aongbucks_user/activity/MainActivity.kt
+++ AongBucks-User/app/src/main/java/com/ssafy/aongbucks_user/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ssafy.aongbucks_user.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ssafy.aongbucks_user.R
+import com.ssafy.aongbucks_user.config.ApplicationClass
 import com.ssafy.aongbucks_user.databinding.ActivityMainBinding
 import com.ssafy.aongbucks_user.fragment.HomeFragment
 import kotlinx.android.synthetic.main.activity_main.*
@@ -30,5 +32,16 @@ class MainActivity : AppCompatActivity() {
     fun hideBottomNav(state : Boolean) {
         if (state) binding.bottomNavigation.visibility = View.GONE
         else binding.bottomNavigation.visibility = View.VISIBLE
+    }
+
+    fun logout() {
+        ApplicationClass.sharedPreferencesUtil.deleteUser()
+
+        val intent = Intent(this, LoginActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+
+        startActivity(intent)
     }
 }
