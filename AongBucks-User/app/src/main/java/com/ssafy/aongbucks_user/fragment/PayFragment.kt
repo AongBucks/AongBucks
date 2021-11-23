e8dc30980adc47e1cf9d348d74916474b6e2e02f
yujin <dldbwls1025@gmail.com>
2021-11-23 11:55:52 +0900

    feat: pay 정보 불러오기

--- AongBucks-User/app/src/main/java/com/ssafy/aongbucks_user/fragment/PayFragment.kt
+++ AongBucks-User/app/src/main/java/com/ssafy/aongbucks_user/fragment/PayFragment.kt
@@ -1,30 +1,32 @@
 package com.ssafy.aongbucks_user.fragment
 
+import android.app.Dialog
+import android.app.ProgressDialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.transition.Slide
-import androidx.transition.Transition
-import androidx.transition.TransitionManager
+import androidx.lifecycle.LiveData
 import com.ssafy.aongbucks_user.R
 import com.ssafy.aongbucks_user.activity.MainActivity
+import com.ssafy.aongbucks_user.config.ApplicationClass
+import com.ssafy.aongbucks_user.databinding.DialogProgressBinding
 import com.ssafy.aongbucks_user.databinding.FragmentPayBinding
+import com.ssafy.aongbucks_user.model.dto.Pay
+import com.ssafy.aongbucks_user.model.dto.User
+import com.ssafy.aongbucks_user.service.PayService
+import com.ssafy.aongbucks_user.util.CommonUtils
 
 class PayFragment : Fragment(){
-
     private lateinit var mainActivity: MainActivity
-
     private lateinit var binding:FragmentPayBinding
+    private lateinit var user: User
+    private lateinit var userPay: LiveData<Pay?>
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -42,7 +44,31 @@ class PayFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        user = ApplicationClass.sharedPreferencesUtil.getUser()
 
+        initPayData()
+    }
+
+    private fun initPayData() {
+        val dialog = Dialog(mainActivity)
+        var dialogBinding : DialogProgressBinding =
+            DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.dialog_progress, null, false)
+        dialogBinding.progressText.text = "Pay 등록 정보를 조회하는 중입니다."
+
+        dialog.apply {
+            setContentView(dialogBinding.root)
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // 배경 둥글게 하기 위함
+
+            userPay = PayService().getPayInfo(user.id)
+            show()
+        }
+
+        userPay.observe(viewLifecycleOwner, {
+            dialog.dismiss()
+            if (userPay.value != null) {
+                binding.priceTextView.text = CommonUtils.makeComma(userPay.value!!.price)
+            }
+        })
     }
 
 }
