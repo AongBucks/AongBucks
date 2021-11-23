a73e8275a65852394370405270ccd4d2ccb59d85
Jeehyun Woo <wjh51333@gmail.com>
2021-11-23 22:08:15 +0900

    서버 grade 수정

--- aongbucks_server/src/main/java/com/ssafy/cafe/controller/rest/UserRestController.java
+++ aongbucks_server/src/main/java/com/ssafy/cafe/controller/rest/UserRestController.java
@@ -2,26 +2,29 @@ package com.ssafy.cafe.controller.rest;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javax.annotation.PostConstruct;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.cafe.model.dto.Grade;
 import com.ssafy.cafe.model.dto.Order;
 import com.ssafy.cafe.model.dto.User;
+import com.ssafy.cafe.model.service.GradeService;
 import com.ssafy.cafe.model.service.OrderService;
 import com.ssafy.cafe.model.service.StampService;
 import com.ssafy.cafe.model.service.UserService;
+
 import io.swagger.annotations.ApiOperation;
 
 @RestController
@@ -38,6 +41,9 @@ public class UserRestController {
 
     @Autowired
     OrderService oService;
+    
+    @Autowired
+    GradeService gService;
 
     @PostMapping
     @ApiOperation(value = "사용자 정보를 추가한다.", response = Boolean.class)
@@ -87,50 +93,20 @@ public class UserRestController {
 
     public Map<String, Object> getGrade(Integer stamp) {
         Map<String, Object> grade = new HashMap<>();
-        int pre = 0;
-        for (Level level : levels) {
-            if (level.max >= stamp) {
-                grade.put("title", level.title);
-                grade.put("img", level.img);
-                if (!level.title.equals("커피나무")) {
-                    int step = (stamp - pre) / level.unit + ((stamp - pre) % level.unit > 0 ? 1 : 0);
-                    grade.put("step", step);
-                    int to = level.unit - (stamp - pre) % level.unit;
+        List<Grade> grades = gService.getAllGrade();
+        for (int i = 0; i < grades.size(); i++) {
+        	Grade g = grades.get(i);
+            if (g.getStandard() <= stamp) {
+                grade.put("title", g.getTitle());
+                grade.put("img", g.getImg());
+                if (i != grades.size() - 1) {
+                    int to = grades.get(i + 1).getStandard() - stamp;
                     grade.put("to", to);
+                    grade.put("next", grades.get(i + 1).getTitle());
                 }
                 break;
-            } else {
-                pre = level.max;
             }
         }
         return grade;
-    }
-
-    private List<Level> levels;
-
-    @PostConstruct
-    public void setup() {
-        levels = new ArrayList<>();
-        levels.add(new Level("씨앗", 10, 50, "seeds.png"));
-        levels.add(new Level("꽃", 15, 125, "flower.png"));
-        levels.add(new Level("열매", 20, 225, "coffee_fruit.png"));
-        levels.add(new Level("커피콩", 25, 350, "coffee_beans.png"));
-        levels.add(new Level("커피나무", Integer.MAX_VALUE, Integer.MAX_VALUE, "coffee_tree.png"));
-    }
-
-
-
-    class Level {
-        private String title;
-        private int unit;
-        private int max;
-        private String img;
-
-        private Level(String title, int unit, int max, String img) {
-            this.title = title;
-            this.unit = unit;
-            this.max = max;
-            this.img = img;
-        }
     }
 }
