1a824b8359cc6acfc0992ee6c4bf36158d506089
Jeehyun Woo <wjh51333@gmail.com>
2021-11-23 11:01:56 +0900

    refactor : 메뉴 디테일 수정

--- AongBucks-User/app/src/main/java/com/ssafy/aongbucks_user/adapter/BindingAdapter.kt
+++ AongBucks-User/app/src/main/java/com/ssafy/aongbucks_user/adapter/BindingAdapter.kt
@@ -1,16 +1,23 @@
 package com.ssafy.aongbucks_user.adapter
 
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.ssafy.aongbucks_user.util.CommonUtils
 
 
-@BindingAdapter("imageDrawable")
-fun imageDrawable(view : View, img : String) {
-//    val resId =
+private const val TAG = "BindingAdapter_싸피"
 
+@BindingAdapter("imageDrawable")
+fun imageDrawable(view : ImageView, img : String?) {
+    val packageName = "com.ssafy.aongbucks_user"
+    val imgName = img?.substring(0, img.length - 4) ?: "coffee1"
+    Log.d(TAG, "imageDrawable: $imgName")
+    val resId = view.resources.getIdentifier(imgName, "drawable", packageName)
+    view.setImageResource(resId)
 }
 
 @BindingAdapter("setRating")
@@ -20,7 +27,7 @@ fun setRating(ratingBar : RatingBar, rating: Double) {
 
 @BindingAdapter("setRatingAvg")
 fun setRatingAvg(view: TextView, ratingAvg : Double) {
-    view.text = String.format("%.0f", ratingAvg)
+    view.text = String.format("%.0lf", ratingAvg)
 }
 
 @BindingAdapter("makeComma")
