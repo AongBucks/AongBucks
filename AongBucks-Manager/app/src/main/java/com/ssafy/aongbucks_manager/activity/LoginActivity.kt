dbb654bd64e7f59f251b96b9c75b4af1906684f8
yujin <dldbwls1025@gmail.com>
2021-11-24 14:38:29 +0900

    feat: 관리자 로그인

--- AongBucks-Manager/app/src/main/java/com/ssafy/aongbucks_manager/activity/LoginActivity.kt
+++ AongBucks-Manager/app/src/main/java/com/ssafy/aongbucks_manager/activity/LoginActivity.kt
@@ -3,10 +3,16 @@ package com.ssafy.aongbucks_manager.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.ssafy.aongbucks_manager.R
 import com.ssafy.aongbucks_manager.databinding.ActivityLoginBinding
 
+private const val TAG = "LoginActivity_싸피"
 class LoginActivity : AppCompatActivity() {
     lateinit var binding:ActivityLoginBinding
 
@@ -15,7 +21,26 @@ class LoginActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
         binding.button.setOnClickListener {
+            checkcode()
+        }
+        binding.codeEditText.setOnEditorActionListener { _, id, _ ->
+            if (id == EditorInfo.IME_ACTION_SEND) {
+                checkcode()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    fun checkcode() {
+        Log.d(TAG, "checkcode: ${binding.codeEditText.text}")
+        if (binding.codeEditText.text.toString().equals("0000")) {
             startActivity(Intent(this, MainActivity::class.java))
+            Toast.makeText(this, "인증되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            Toast.makeText(this, "관리자 코드를 확인해주세요.", Toast.LENGTH_SHORT).show()
         }
     }
 }
